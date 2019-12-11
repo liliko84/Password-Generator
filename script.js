f88b26dab610c48f9191c3ffba525fe5153c8f63
lili gugushvili <lilikog@yahoo.com>
2019-12-10 20:24:22 -0500

    all files

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("hello")
+var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","_","+","=","-","/","~","}","{","]","[",">",".",",",":",";","?"];
+console.log(arraySpecialChaacters);
+var numberCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
